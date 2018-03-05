6e17d132a68063005c9255d186dbacacfec29c5e
Vivienne <vpatward@caltech.edu>
2018-03-04 16:02:51 -0800

    init

---
+++ player.hpp
@@ -0,0 +1,21 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include <iostream>
+#include "common.hpp"
+#include "board.hpp"
+using namespace std;
+
+class Player {
+
+public:
+    Player(Side side);
+    ~Player();
+
+    Move *doMove(Move *opponentsMove, int msLeft);
+
+    // Flag to tell if the player is running within the test_minimax context
+    bool testingMinimax;
+};
+
+#endif
